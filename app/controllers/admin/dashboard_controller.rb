2b8db809b23bc48ea3a30db185b5d8c09cd5ee72
Daniel Tan <danieltan380@gmail.com>
2023-11-15 20:11:20 -0800

    Add number of products and categories to the admin Dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 
   private
