9f3da63350898a7a54fb0e80f83fe8b71e4d3671
Daniel Tan <danieltan380@gmail.com>
2023-11-15 19:29:23 -0800

    Add HTTP basic authentication to restrict access to Admin section

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
 
   def index
     @products = Product.order(id: :desc).all
@@ -36,5 +37,12 @@ class Admin::ProductsController < ApplicationController
       :price
     )
   end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['HTTP_BASIC_AUTH_USERNAME'] && password == ENV['HTTP_BASIC_AUTH_PASSWORD']
+    end
+  end
+
 
 end
