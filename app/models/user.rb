7a77f9e9e2f5a27da3b2464f4d887714053adba2
Daniel Tan <danieltan380@gmail.com>
2023-11-20 15:43:53 -0800

    Add validations and authenticate_with_credentials to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ApplicationRecord
 
   has_secure_password
+
+  validates :email, presence: true, uniqueness: {case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @email = User.all.map{|user| user.email}
+    @user = User.find_by_email(email.strip.downcase)
+
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+    
+  end
   
 end
