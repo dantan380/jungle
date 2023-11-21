c2ce09ac4dd7fd7b0673f6bc5db18d17c8d16374
Daniel Tan <danieltan380@gmail.com>
2023-11-21 13:16:44 -0800

    Add test to check if there are multiple products on the page

--- cypress/integration/1-getting-started/home.spec.js
+++ cypress/integration/1-getting-started/home.spec.js
@@ -6,4 +6,8 @@ describe('Products on Home page', () => {
   it("There are products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+
+  it("There are multiple products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
 })
